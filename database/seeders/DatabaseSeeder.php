e16cdf03d551260c52809ffb526dc81b185d8826
Marosy Gergo <garry955@gmail.com>
2024-05-07 13:41:51 +0200

    create product seeder and add migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,17 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        for ($i = 0; $i < 10; $i++) {
+            Product::create(
+                [
+                    "name" => "Product-" . $i,
+                    "description" => "Random description" . $i,
+                    "link" => "#",
+                    "price" => $i * 1000,
+                    "stock" => $i
+
+                ]
+            );
+        }
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,18 @@
 
         @include('partials.hero')
 
-        <x-product
- {{-- :product=”$product” --}}
-   /> 
+        <div class="mb-20">
+            <h2 class="text-3xl font-bold mb-10">Termékek</h2>
+            <div class="products px-5">
+                @for ($i = 0; $i < 8; $i++)
+                    <x-product
+                    {{-- :product=”$product” --}}
+                    /> 
+                @endfor
+
+            </div>
+
+        </div>
     </section>
 
 </x-layout>
